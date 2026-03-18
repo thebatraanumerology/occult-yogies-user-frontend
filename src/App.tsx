fcdea54189621c29542e879727daa2ec6e0fe32a
KumarVivekPathak <coolvivekpathak2004@gmail.com>
2026-03-18 20:12:00 +0530

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/login/Login'
 import VastuAnalysis from './pages/vastu-energy/VastuAnalysis'
 import Home from './pages/home/Home'
+import LoginLayout from './layout/LoginLayout'
+import DefaultLayout from './layout/DefaultLayout'
 
 const App : React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/vastu-analysis" element={<VastuAnalysis />} />
+        <Route element={<LoginLayout />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/vastu-analysis" element={<VastuAnalysis />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
