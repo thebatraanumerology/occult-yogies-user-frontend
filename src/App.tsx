7295ec5fb506c29c5ccc47c028efdfc40c61f786
KumarVivekPathak <coolvivekpathak2004@gmail.com>
2026-03-19 14:30:40 +0530

    set up an dlogin frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/login/Login'
-import VastuAnalysis from './pages/vastu-energy/VastuAnalysis'
+import VastuAnalysis from './pages/energy-vastu/VastuAnalysis'
 import Home from './pages/home/Home'
 import LoginLayout from './layout/LoginLayout'
 import DefaultLayout from './layout/DefaultLayout'
