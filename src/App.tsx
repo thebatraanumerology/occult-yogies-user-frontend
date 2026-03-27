6ecdc093e7935245ac39f0b03f595d4fc2393e0f
KumarVivekPathak <coolvivekpathak2004@gmail.com>
2026-03-27 15:35:01 +0530

    unwanted url not found integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NotFound from './pages/NotFound';
 import Login from './pages/login/login'
 import VastuAnalysis from './pages/energy-vastu'
 import Home from './pages/home/Home'
@@ -8,13 +8,13 @@ import DefaultLayout from './layout/DefaultLayout'
 import EnergyVastuAnalyse from './pages/energy-vastu/analyse'
 import EnergyVastuList from './pages/energy-vastu/list'
 
-const App : React.FC = () => {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<LoginLayout />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
+          <Route path="/login" element={<Login />} />
+        </Route>
 
         <Route element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
@@ -22,9 +22,11 @@ const App : React.FC = () => {
           <Route path="/energy-vastu/:id" element={<EnergyVastuAnalyse />} />
           <Route path="/list/energy-vastu" element={<EnergyVastuList />} />
         </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
